beb8348e717c0a5c25e0f363e568379b14b1040d
lahdissou97 <lahdissouahmed1997@gmail.com>
2020-05-24 03:54:36 +0200

    STOCKAGE DES DONNEE EN CACHE

---
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class Constants {
+    public static final java.lang.String KEY_POKEMON_LIST = ;
+    Static String KEY_POKEMON_LIST ="jsonPokemonList";
+}
